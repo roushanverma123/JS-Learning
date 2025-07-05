52926fad5295ae3a5de0b271b0f4681fbf5a69e0
Roushan Verma <roushanverma123@gmail.com>
2025-07-05 19:27:24 +0530

    conditional statements

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# JS-Learning
+# Revising Javascript
---
+++ conditions.js
@@ -0,0 +1,43 @@
+//check if a number is greater or not:
+let num1 = 5;
+let num2 = 8;
+if (num1 > num2) {
+  console.log("num1 is bigger ", num1);
+} else {
+  console.log("num2 is bigger ", num2);
+}
+
+//check if usernames are same or not (strings are same or not basically)
+let username1 = "roushanverma";
+let username2 = "rverma";
+
+if (username1 === username2) {
+  console.log("usernames are equal");
+} else {
+  console.log("usernames are different");
+}
+
+//check if a variable is a number or not
+let score = 44;
+if (typeof score === "number") {
+  console.log("yes this is number");
+} else {
+  console.log("no that is not a number");
+}
+
+//check if the boolean is true or false
+let isReady = true;
+if (isReady) {
+  console.log("yes it is ready");
+} else {
+  console.log("yes it is true");
+}
+
+//check if an array is empty or not
+let items = [];
+if (items.length === 0) {
+  console.log("empty array");
+} else {
+  console.log("not an empty array");
+}
+
